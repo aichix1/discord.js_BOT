627f2fb2935726290f14d6a0dc8b90b640e08f0c
aichix1 <hiroki88476@gmail.com>
2023-11-12 18:31:49 +0900

    Clean

--- replit.nix
+++ replit.nix
@@ -11,21 +11,8 @@
 		pkgs.libsndfile
 		pkgs.vim
 		pkgs.tesseract
-		#pkgs.replitPackages.prybar-python310
-		#pkgs.nodePackages.typescript-language-server
-		#pkgs.replitPackages.jest
-  ];
-  env = {
-    #PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-    #  pkgs.stdenv.cc.cc.lib
-    #  pkgs.zlib
-    #  pkgs.glib
-    #  pkgs.xorg.libX11
-    #];
-    #PYTHONHOME = "${pkgs.python310Full}";
-    #PYTHONBIN = "${pkgs.python310Full}/bin/python3.10";
-    LANG = "en_US.UTF-8";
-    #STDERREDBIN = "${pkgs.replitPackages.stderred}/bin/stderred";
-    #PRYBAR_PYTHON_BIN = "${pkgs.replitPackages.prybar-python310}/bin/prybar-python310";
-  };
+	];
+	env = {
+		LANG = "en_US.UTF-8";
+	};
 }
