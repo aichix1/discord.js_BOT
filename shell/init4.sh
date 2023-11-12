dfcf3f327e4e1ddcf66a785285bfb762f5a8a9c4
aichix1 <hiroki88476@gmail.com>
2023-11-12 20:15:53 +0900

    Clean

---
+++ shell/init4.sh
@@ -0,0 +1,9 @@
+npm install node-opus
+npm install ffmpeg-static
+npm install tweetnacl
+npm install js-yaml
+npm install express
+npm install serve-favicon
+npm install gaxios
+#npm install moment
+npm install moment-timezone
---
+++ shell/init5.sh
@@ -0,0 +1,6 @@
+npm install google-translate-api-x
+npm install https-proxy-agent
+npm install node-cron
+npm install python-shell
+
+npm install discord.js
