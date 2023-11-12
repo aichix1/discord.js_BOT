68bcb30947ee009b08b3c06a9eadb8a92e9036ad
aichix1 <hiroki88476@gmail.com>
2023-11-12 18:40:50 +0900

    Clean

--- replit.nix
+++ replit.nix
@@ -5,12 +5,8 @@
 		pkgs.gh
 		pkgs.nodejs_20
 		pkgs.python311Full
-		pkgs.yarn
 		pkgs.replitPackages.stderred
-		pkgs.p7zip
 		pkgs.libsndfile
-		pkgs.vim
-		pkgs.tesseract
 	];
 	env = {
 		LANG = "en_US.UTF-8";
