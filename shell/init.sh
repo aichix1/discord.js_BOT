6a47805a267dc1db335a01e947e87cca54bbfd33
aichix1 <hiroki88476@gmail.com>
2023-11-12 20:07:20 +0900

    Clean

--- shell/init.sh
+++ shell/init.sh
@@ -2,9 +2,3 @@ npm install -g npm
 python -m venv Python
 curl -kL https://bootstrap.pypa.io/get-pip.py > Python/user/get-pip.py
 python Python/user/get-pip.py
-pip install edge-tts
-pip install gTTS
-mkdir data/sounds
-mkdir data/log
-mkdir data/log/log
-npm install
