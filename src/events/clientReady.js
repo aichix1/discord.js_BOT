b48ad4476f315286297c80e12d0c2b32fd008464
aichix1 <hiroki88476@gmail.com>
2023-11-12 18:53:15 +0900

    Crean

--- src/events/clientReady.js
+++ src/events/clientReady.js
@@ -8,5 +8,5 @@ export const once = true;
 export async function execute(interaction, client) {
 	const log = new Logger(this.name);
 	log.info(`Ready! Logged in as ${client.user.tag}`);
-	await client.channels.resolve(env.Stage)?.send('startup');
+	//await client.channels.resolve(env.Stage)?.send('startup');
 }
